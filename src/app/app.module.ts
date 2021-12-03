9937b1fd4b51e739958522c79da5494f6f57ca01
Sandro37 <alessandro37@outlook.com.br>
2021-12-02 22:17:03 -0300

    routes. route link

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MaterializeModule } from 'angular2-materialize';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { LoginComponent } from './login/login.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CursosComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
